e1b0797b536cbbd3b82de854d41235a00488a59f
Spike-Fu <meiwenfu@usc.edu>
2022-09-25 16:18:53 -0700

    asd

--- Fall22_assignment1/Fall22_assignment1/assign1_starterCode_windowsVS2017/assign1/assign1/assign1.cpp
+++ Fall22_assignment1/Fall22_assignment1/assign1_starterCode_windowsVS2017/assign1/assign1/assign1.cpp
@@ -7,8 +7,6 @@
   <your name>
 */
 
-
-
 #include "stdafx.h"
 #include <windows.h>
 #include <stdlib.h>
@@ -21,11 +19,16 @@ using namespace cimg_library;
 int g_iMenuId;
 
 int g_vMousePos[2] = {0, 0};
-int g_iLeftMouseButton = 0;    /* 1 if pressed, 0 if not */
+int g_iLeftMouseButton = 0; /* 1 if pressed, 0 if not */
 int g_iMiddleMouseButton = 0;
 int g_iRightMouseButton = 0;
 
-typedef enum { ROTATE, TRANSLATE, SCALE } CONTROLSTATE;
+typedef enum
+{
+  ROTATE,
+  TRANSLATE,
+  SCALE
+} CONTROLSTATE;
 
 CONTROLSTATE g_ControlState = ROTATE;
 
@@ -40,12 +43,11 @@ CImg<unsigned char> *g_pHeightData;
 /* Please install ImageMagick and replace the path below to the correct path to convert.exe on your computer */
 void initializeImageMagick()
 {
-	cimg::imagemagick_path("convert.exe", true);
+  cimg::imagemagick_path("convert.exe", true);
 }
 
-
 /* Write a screenshot to the specified filename */
-void saveScreenshot (char *filename)
+void saveScreenshot(char *filename)
 {
   int i, j;
 
@@ -57,8 +59,9 @@ void saveScreenshot (char *filename)
 
   printf("File to save to: %s\n", filename);
 
-  for (i=479; i>=0; i--) {
-    glReadPixels(0, 479-i, 640, 1, GL_RGB, GL_UNSIGNED_BYTE,
+  for (i = 479; i >= 0; i--)
+  {
+    glReadPixels(0, 479 - i, 640, 1, GL_RGB, GL_UNSIGNED_BYTE,
                  in.data());
   }
 
@@ -66,7 +69,6 @@ void saveScreenshot (char *filename)
     printf("File saved Successfully\n");
   else
     printf("Error in Saving\n");
-
 }
 
 void myinit()
@@ -79,7 +81,7 @@ void display()
 {
   /* draw 1x1 cube about origin */
   /* replace this code with your height field implementation */
-  /* you may also want to precede it with your 
+  /* you may also want to precede it with your
 rotation/translation/scaling */
 
   glBegin(GL_POLYGON);
@@ -100,9 +102,9 @@ void menufunc(int value)
 {
   switch (value)
   {
-    case 0:
-      exit(0);
-      break;
+  case 0:
+    exit(0);
+    break;
   }
 }
 
@@ -114,47 +116,47 @@ void doIdle()
   glutPostRedisplay();
 }
 
-/* converts mouse drags into information about 
+/* converts mouse drags into information about
 rotation/translation/scaling */
 void mousedrag(int x, int y)
 {
-  int vMouseDelta[2] = {x-g_vMousePos[0], y-g_vMousePos[1]};
-  
+  int vMouseDelta[2] = {x - g_vMousePos[0], y - g_vMousePos[1]};
+
   switch (g_ControlState)
   {
-    case TRANSLATE:  
-      if (g_iLeftMouseButton)
-      {
-        g_vLandTranslate[0] += vMouseDelta[0]*0.01;
-        g_vLandTranslate[1] -= vMouseDelta[1]*0.01;
-      }
-      if (g_iMiddleMouseButton)
-      {
-        g_vLandTranslate[2] += vMouseDelta[1]*0.01;
-      }
-      break;
-    case ROTATE:
-      if (g_iLeftMouseButton)
-      {
-        g_vLandRotate[0] += vMouseDelta[1];
-        g_vLandRotate[1] += vMouseDelta[0];
-      }
-      if (g_iMiddleMouseButton)
-      {
-        g_vLandRotate[2] += vMouseDelta[1];
-      }
-      break;
-    case SCALE:
-      if (g_iLeftMouseButton)
-      {
-        g_vLandScale[0] *= 1.0+vMouseDelta[0]*0.01;
-        g_vLandScale[1] *= 1.0-vMouseDelta[1]*0.01;
-      }
-      if (g_iMiddleMouseButton)
-      {
-        g_vLandScale[2] *= 1.0-vMouseDelta[1]*0.01;
-      }
-      break;
+  case TRANSLATE:
+    if (g_iLeftMouseButton)
+    {
+      g_vLandTranslate[0] += vMouseDelta[0] * 0.01;
+      g_vLandTranslate[1] -= vMouseDelta[1] * 0.01;
+    }
+    if (g_iMiddleMouseButton)
+    {
+      g_vLandTranslate[2] += vMouseDelta[1] * 0.01;
+    }
+    break;
+  case ROTATE:
+    if (g_iLeftMouseButton)
+    {
+      g_vLandRotate[0] += vMouseDelta[1];
+      g_vLandRotate[1] += vMouseDelta[0];
+    }
+    if (g_iMiddleMouseButton)
+    {
+      g_vLandRotate[2] += vMouseDelta[1];
+    }
+    break;
+  case SCALE:
+    if (g_iLeftMouseButton)
+    {
+      g_vLandScale[0] *= 1.0 + vMouseDelta[0] * 0.01;
+      g_vLandScale[1] *= 1.0 - vMouseDelta[1] * 0.01;
+    }
+    if (g_iMiddleMouseButton)
+    {
+      g_vLandScale[2] *= 1.0 - vMouseDelta[1] * 0.01;
+    }
+    break;
   }
   g_vMousePos[0] = x;
   g_vMousePos[1] = y;
@@ -171,88 +173,88 @@ void mousebutton(int button, int state, int x, int y)
 
   switch (button)
   {
-    case GLUT_LEFT_BUTTON:
-      g_iLeftMouseButton = (state==GLUT_DOWN);
-      break;
-    case GLUT_MIDDLE_BUTTON:
-      g_iMiddleMouseButton = (state==GLUT_DOWN);
-      break;
-    case GLUT_RIGHT_BUTTON:
-      g_iRightMouseButton = (state==GLUT_DOWN);
-      break;
+  case GLUT_LEFT_BUTTON:
+    g_iLeftMouseButton = (state == GLUT_DOWN);
+    break;
+  case GLUT_MIDDLE_BUTTON:
+    g_iMiddleMouseButton = (state == GLUT_DOWN);
+    break;
+  case GLUT_RIGHT_BUTTON:
+    g_iRightMouseButton = (state == GLUT_DOWN);
+    break;
   }
- 
-  switch(glutGetModifiers())
+
+  switch (glutGetModifiers())
   {
-    case GLUT_ACTIVE_CTRL:
-      g_ControlState = TRANSLATE;
-      break;
-    case GLUT_ACTIVE_SHIFT:
-      g_ControlState = SCALE;
-      break;
-    default:
-      g_ControlState = ROTATE;
-      break;
+  case GLUT_ACTIVE_CTRL:
+    g_ControlState = TRANSLATE;
+    break;
+  case GLUT_ACTIVE_SHIFT:
+    g_ControlState = SCALE;
+    break;
+  default:
+    g_ControlState = ROTATE;
+    break;
   }
 
   g_vMousePos[0] = x;
   g_vMousePos[1] = y;
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	// I've set the argv[1] to spiral.jpg.
-	// To change it, on the "Solution Explorer",
-	// right click "assign1", choose "Properties",
-	// go to "Configuration Properties", click "Debugging",
-	// then type your texture name for the "Command Arguments"
-	if (argc<2)
-	{  
-		printf ("usage: %s heightfield.jpg\n", argv[0]);
-		exit(1);
-	}
+  // I've set the argv[1] to spiral.jpg.
+  // To change it, on the "Solution Explorer",
+  // right click "assign1", choose "Properties",
+  // go to "Configuration Properties", click "Debugging",
+  // then type your texture name for the "Command Arguments"
+  if (argc < 2)
+  {
+    printf("usage: %s heightfield.jpg\n", argv[0]);
+    exit(1);
+  }
 
-	initializeImageMagick();
+  initializeImageMagick();
 
-	g_pHeightData = new CImg<unsigned char>((char*)argv[1]);
-	if (!g_pHeightData)
-	{
-	    printf ("error reading %s.\n", argv[1]);
-	    exit(1);
-	}
+  g_pHeightData = new CImg<unsigned char>((char *)argv[1]);
+  if (!g_pHeightData)
+  {
+    printf("error reading %s.\n", argv[1]);
+    exit(1);
+  }
 
-	glutInit(&argc,(char**)argv);
-  
-	/*
-		create a window here..should be double buffered and use depth testing
-  
-	    the code past here will segfault if you don't have a window set up....
-	    replace the exit once you add those calls.
-	*/
-	exit(0);
+  glutInit(&argc, (char **)argv);
 
-	/* tells glut to use a particular display function to redraw */
-	glutDisplayFunc(display);
-  
-	/* allow the user to quit using the right mouse button menu */
-	g_iMenuId = glutCreateMenu(menufunc);
-	glutSetMenu(g_iMenuId);
-	glutAddMenuEntry("Quit",0);
-	glutAttachMenu(GLUT_RIGHT_BUTTON);
-  
-	/* replace with any animate code */
-	glutIdleFunc(doIdle);
+  /*
+    create a window here..should be double buffered and use depth testing
 
-	/* callback for mouse drags */
-	glutMotionFunc(mousedrag);
-	/* callback for idle mouse movement */
-	glutPassiveMotionFunc(mouseidle);
-	/* callback for mouse button changes */
-	glutMouseFunc(mousebutton);
+      the code past here will segfault if you don't have a window set up....
+      replace the exit once you add those calls.
+  */
+  exit(0);
 
-	/* do initialization */
-	myinit();
+  /* tells glut to use a particular display function to redraw */
+  glutDisplayFunc(display);
 
-	glutMainLoop();
-	return 0;
+  /* allow the user to quit using the right mouse button menu */
+  g_iMenuId = glutCreateMenu(menufunc);
+  glutSetMenu(g_iMenuId);
+  glutAddMenuEntry("Quit", 0);
+  glutAttachMenu(GLUT_RIGHT_BUTTON);
+
+  /* replace with any animate code */
+  glutIdleFunc(doIdle);
+
+  /* callback for mouse drags */
+  glutMotionFunc(mousedrag);
+  /* callback for idle mouse movement */
+  glutPassiveMotionFunc(mouseidle);
+  /* callback for mouse button changes */
+  glutMouseFunc(mousebutton);
+
+  /* do initialization */
+  myinit();
+
+  glutMainLoop();
+  return 0;
 }
